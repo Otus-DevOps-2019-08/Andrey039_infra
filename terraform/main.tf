83c663205f4cc53856e31f0eca4b8e256408e4fb
Andrey Maslov <maslov.kng@gmail.com>
2019-10-14 20:30:37 +0200

    @#$

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,13 @@
 terraform {
   # Версия terraform
-  required_version = "0.12.8"
+  required_version = "~> 0.12"
+  required_providers {
+    google = "~>2.7"
+  }
 }
-
 provider "google" {
   # Версия провайдера
-  version = "2.15.0"
+  version = "2.15"
 
   # ID проекта
   project = var.project
