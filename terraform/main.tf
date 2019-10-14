8e1fdd5331654dda371c5268533d8079dbec95ca
Andrey Maslov <maslov.kng@gmail.com>
2019-10-14 20:26:42 +0200

    @#$

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ terraform {
 
 provider "google" {
   # Версия провайдера
-  version = "2.15"
+  version = "2.15.0"
 
   # ID проекта
   project = var.project
@@ -66,12 +66,12 @@ resource "google_compute_instance" "app" {
 
 }
 
-resource "google_compute_project_metadata" "ssh-keys" {
-  metadata = {
-    ssh-keys = <<EOF
-    appuser1:${file(var.public_key_path)}
-    appuser2:${file(var.public_key_path)}
-    appuser3:${file(var.public_key_path)}
-EOF
-  }
+#resource "google_compute_project_metadata" "ssh-keys" {
+#  metadata = {
+#    ssh-keys = <<EOF
+#    appuser1:${file(var.public_key_path)}
+#    appuser2:${file(var.public_key_path)}
+#    appuser3:${file(var.public_key_path)}
+#EOF
+#  }
 }
