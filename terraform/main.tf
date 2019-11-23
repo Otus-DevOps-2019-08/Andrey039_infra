e06acb98664d84d641884f404b2685a48e12778d
Andrey Maslov <maslov.kng@gmail.com>
2019-11-23 22:51:54 +0200

    Modules

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,4 @@
-terraform {
-  # Версия terraform
-  required_version = "~> 0.12"
-  required_providers {
-    google = "~>2.7"
-  }
-}
+
 provider "google" {
   # Версия провайдера
   version = "2.15"
@@ -12,4 +6,18 @@ provider "google" {
   # ID проекта
   project = var.project
   region  = var.region
+}
+
+module "app" {
+  source          = "./modules/app"
+  public_key_path = var.public_key_path
+  zone            = var.zone
+  app_disk_image  = var.app_disk_image
+}
+
+module "db" {
+  source          = "./modules/db"
+  public_key_path = var.public_key_path
+  zone            = var.zone
+  db_disk_image   = var.db_disk_image
 }
